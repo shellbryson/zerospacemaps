9fd97dc0f2249c4171105f409257894338265166
Shell Bryson <shell@shellbryson.com>
2021-08-20 23:09:00 +0100

    feat(map): alternative experiment

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,34 +1,48 @@
 import React, { useRef, useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 
-import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import ReactMapGL from "react-map-gl";
+
+// import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
 import { mapState } from "../store";
 
-mapboxgl.accessToken =
-  "pk.eyJ1Ijoic2hlbGxicnlzb24iLCJhIjoiY2tzajQ5NXVyMGdhdzJwbmNnYXJuNHJoeiJ9.uXFj0pmv3_9F1hebVu8CWA";
+// mapboxgl.accessToken =
+//   "pk.eyJ1Ijoic2hlbGxicnlzb24iLCJhIjoiY2tzajQ5NXVyMGdhdzJwbmNnYXJuNHJoeiJ9.uXFj0pmv3_9F1hebVu8CWA";
 
 export default function Map() {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
+  // const mapContainer = useRef(null);
+  // const map = useRef(null);
   const [lng, setLng] = useState(-3.188267);
   const [lat, setLat] = useState(55.953251);
-  const [zoom, setZoom] = useState(9);
+  // const [zoom, setZoom] = useState(9);
+
+
+  const [viewport, setViewport] = useState({
+    width: "100%",
+    height: "100%",
+    latitude: 55.953251,
+    longitude: -3.188267,
+    zoom: 8,
+  });
+
 
   const [currentMapState, setMapState] = useRecoilState(mapState);
 
   const onChangeCoords = () => {
-    if (!map.current) return; // wait for map to initialize
+    // if (!map.current) return; // wait for map to initialize
 
-    map.current.setCenter({
-      lng: lng,
-      lat: lat,
-    });
+    // map.current.setCenter({
+    //   lng: lng,
+    //   lat: lat,
+    // });
 
     setMapState({
-      lng: lng,
-      lat: lat,
-      zoom: map.current.getZoom(),
+      lng: viewport.longitude,
+      lat: viewport.latitude,
+      zoom: viewport.zoom,
     });
+
+    setViewport({ ...viewport, longitude: parseFloat(lng), latitude: parseFloat(lat) });
 
   };
 
@@ -40,80 +54,86 @@ export default function Map() {
     setLat(e.target.value);
   };
 
-  const onZoomOut = () => {
-    if (!map.current) return; // wait for map to initialize
+  // const onZoomOut = () => {
+  //   if (!map.current) return; // wait for map to initialize
 
-    let z = map.current.getZoom();
-    z = z - 0.1;
+  //   let z = map.current.getZoom();
+  //   z = z - 0.1;
 
-    map.current.zoomTo(z);
+  //   map.current.zoomTo(z);
 
-    setMapState({
-      lng: lng,
-      lat: lat,
-      zoom: map.current.getZoom(),
-    });
-  };
+  //   setMapState({
+  //     lng: lng,
+  //     lat: lat,
+  //     zoom: map.current.getZoom(),
+  //   });
+  // };
 
-  const onZoomIn = () => {
-    if (!map.current) return; // wait for map to initialize
+  // const onZoomIn = () => {
+  //   if (!map.current) return; // wait for map to initialize
 
-    let z = map.current.getZoom();
-    z = z + 0.1;
+  //   let z = map.current.getZoom();
+  //   z = z + 0.1;
 
-    map.current.zoomTo(z);
+  //   map.current.zoomTo(z);
 
-    setMapState({
-      lng: lng,
-      lat: lat,
-      zoom: map.current.getZoom(),
-    });
-  };
+  //   setMapState({
+  //     lng: lng,
+  //     lat: lat,
+  //     zoom: map.current.getZoom(),
+  //   });
+  // };
 
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/mapbox/streets-v11",
-      center: [lng, lat],
-      zoom: zoom,
-    });
-  });
+  // useEffect(() => {
+  //   if (map.current) return; // initialize map only once
+  //   map.current = new mapboxgl.Map({
+  //     container: mapContainer.current,
+  //     style: "mapbox://styles/mapbox/streets-v11",
+  //     center: [lng, lat],
+  //     zoom: zoom,
+  //   });
+  // });
 
-  useEffect(() => {
-    if (!map.current) return; // wait for map to initialize
-    map.current.on("move", () => {
+  // useEffect(() => {
+  //   if (!map.current) return; // wait for map to initialize
+  //   map.current.on("move", () => {
 
-      const _lng = map.current.getCenter().lng.toFixed(6);
-      const _lat = map.current.getCenter().lat.toFixed(6);
-      //const _zoom = map.current.getZoom().toFixed(2);
+  //     const _lng = map.current.getCenter().lng.toFixed(6);
+  //     const _lat = map.current.getCenter().lat.toFixed(6);
+  //     //const _zoom = map.current.getZoom().toFixed(2);
 
-      setLng(_lng);
-      setLat(_lat);
-      //setZoom(_zoom);
+  //     setLng(_lng);
+  //     setLat(_lat);
+  //     //setZoom(_zoom);
 
-      // setMapState({
-      //   lng: _lng,
-      //   lat: _lat,
-      //   zoom: _zoom,
-      // });
+  //     // setMapState({
+  //     //   lng: _lng,
+  //     //   lat: _lat,
+  //     //   zoom: _zoom,
+  //     // });
 
-    });
-  });
+  //   });
+  // });
 
   return (
     <div className="zerospace-map">
       <div className="zerospace-meta">
-        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+        {/* Longitude: {lng} | Latitude: {lat} | Zoom: {zoom} */}
         <label htmlFor="lng">Long:</label>
         <input id="lng" type="text" value={lng} onChange={onChangeLng} />
         <label htmlFor="lat">Lat:</label>
         <input id="lat" type="text" value={lat} onChange={onChangeLat} />
         <button onClick={onChangeCoords}>Go</button>
-        <button onClick={onZoomIn}>+</button>
-        <button onClick={onZoomOut}>-</button>
+        {/* <button onClick={onZoomIn}>+</button>
+        <button onClick={onZoomOut}>-</button> */}
       </div>
-      <div ref={mapContainer} className="zerospace-map__canvas" />
+      {/* <div ref={mapContainer} className="zerospace-map__canvas" /> */}
+
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken="pk.eyJ1Ijoic2hlbGxicnlzb24iLCJhIjoiY2tzajQ5NXVyMGdhdzJwbmNnYXJuNHJoeiJ9.uXFj0pmv3_9F1hebVu8CWA"
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}
+      />
     </div>
   );
 }
